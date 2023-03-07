39d244006735fd84841101733183734e9bc69815
yeosiowvic <victoryeo1974@gmail.com>
2023-03-07 12:23:24 +0800

    add nestjs mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HealthModule } from './modules/health/health.module';
 import { LoggingInterceptor } from './common/logging.interceptor';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { BooksModule } from './modules/books/books.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost/book'),
     ConfigModule.forRoot({
       expandVariables: true,
     }),
     HealthModule,
+    BooksModule
   ],
   controllers: [],
   providers: [
