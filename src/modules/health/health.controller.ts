81c0638f009cf49dfef4c6f5ae16212d61345827
yeosiowvic <victoryeo1974@gmail.com>
2023-03-13 15:24:16 +0800

    add typescript class test code

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -4,6 +4,24 @@ import { Controller, Get } from '@nestjs/common';
 export class HealthController {
   @Get()
   getHealthCheck() {
+
+    // can instantiate an object from a class
+    class Pizza {
+      constructor(public name: string, public toppings: string[]) {}
+    }
+    // PizzaMaker is a singleton class due to static method
+    class PizzaMaker {
+      static create(event: Pizza) {
+        return new Pizza(event.name, event.toppings);
+      }
+    }
+    
+    const pizza = PizzaMaker.create({
+      name: 'Inferno',
+      toppings: ['cheese', 'peppers'],
+    });
+    console.log(pizza);
+    
     return 'healthy';
   }
 }
